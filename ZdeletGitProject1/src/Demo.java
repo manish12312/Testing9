430ed1c2a751c2d06ad5a773529520fe8ffad561
MANISH <MANISH@DESKTOP-NPV50CU>
2021-06-03 11:08:45 +0530

    Submitted by me

---
+++ ZdeletGitProject1/src/Demo.java
@@ -0,0 +1,46 @@
+
+public class Demo 
+{
+	
+	
+	public static void main(String[] args)
+	{
+		System.out.println(" hello demo ");
+		Demo dm=new Demo();
+		dm.m10();
+		dm.add();
+		
+		
+	}
+	
+	
+	public void m10()
+	{
+		System.out.println(" M 10 ");
+	}
+	public void add()
+	{
+		System.out.println(" Hello ADD");
+		
+	}
+	
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
